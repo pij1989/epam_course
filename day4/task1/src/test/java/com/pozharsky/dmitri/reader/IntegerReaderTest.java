ab287b9fcc2a70b0efec386f72d6ac4b2aacd56f
pij1989 <dimon.pij@mail.ru>
2020-11-22 19:11:19 +0300

    done tasks

---
+++ day4/task1/src/test/java/com/pozharsky/dmitri/reader/IntegerReaderTest.java
@@ -0,0 +1,29 @@
+package com.pozharsky.dmitri.reader;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class IntegerReaderTest {
+    static final String FILE = "..\\data\\integers.txt";
+    IntegerReader integerReader;
+
+    @BeforeMethod
+    public void setUp() {
+        integerReader = new IntegerReader();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        integerReader = null;
+    }
+
+    @Test
+    public void testReadIntegers() {
+        int[] actual = integerReader.readIntegers(FILE);
+        int[] expect = new int[]{1, 2, 30, 40, 60, 70, 3, 75, 9, 8, 7, 5};
+        assertEquals(actual, expect);
+    }
+}
--- day4/task1/src/test/java/com/pozharsky/dmitri/service/IntegerArrayServiceTest.java
+++ day4/task1/src/test/java/com/pozharsky/dmitri/service/IntegerArrayServiceTest.java
@@ -1,54 +1,94 @@
 package com.pozharsky.dmitri.service;
 
+import com.pozharsky.dmitri.entity.IntegerArray;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 public class IntegerArrayServiceTest {
+    IntegerArrayService integerArrayService;
+    IntegerArray integerArray;
 
     @BeforeMethod
     public void setUp() {
+        integerArrayService = new IntegerArrayService();
+        integerArray = new IntegerArray(13, 5, 8, 831, 287, 775, 557, 31, 21);
     }
 
     @AfterMethod
     public void tearDown() {
+        integerArrayService = null;
+        integerArray = null;
     }
 
     @Test
     public void testQuickSort() {
+        IntegerArray expect = new IntegerArray(5, 8, 13, 21, 31, 287, 557, 775, 831);
+        integerArrayService.quickSort(integerArray);
+        IntegerArray actual = integerArray;
+        assertEquals(actual, expect);
     }
 
     @Test
     public void testBubleSort() {
+        IntegerArray expect = new IntegerArray(5, 8, 13, 21, 31, 287, 557, 775, 831);
+        integerArrayService.bubleSort(integerArray);
+        IntegerArray actual = integerArray;
+        assertEquals(actual, expect);
     }
 
     @Test
     public void testMergeSort() {
+        integerArrayService.mergeSort(integerArray);
+        IntegerArray actual = integerArray;
+        IntegerArray expect = new IntegerArray(5, 8, 13, 21, 31, 287, 557, 775, 831);
+        assertEquals(actual, expect);
     }
 
     @Test
     public void testBinarySearch() {
+        integerArrayService.mergeSort(integerArray);
+        int actual = integerArrayService.binarySearch(integerArray, 21);
+        int expect = 3;
+        assertEquals(actual, expect);
     }
 
     @Test
     public void testMax() {
+        int actual = integerArrayService.max(integerArray);
+        int expect = 831;
+        assertEquals(actual, expect);
     }
 
     @Test
     public void testMin() {
+        int actual = integerArrayService.min(integerArray);
+        int expect = 5;
+        assertEquals(actual, expect);
     }
 
     @Test
     public void testPrimeNumbers() {
+        List<Integer> actual = integerArrayService.primeNumbers(integerArray);
+        List<Integer> expect = List.of(13, 5, 557, 31);
+        assertEquals(actual, expect);
     }
 
     @Test
     public void testFibonaciNumbers() {
+        List<Integer> actual = integerArrayService.fibonaciNumbers(integerArray);
+        List<Integer> expect = List.of(13, 5, 8, 21);
+        assertEquals(actual, expect);
     }
 
     @Test
     public void testDefineThreeDigitNumberWithoutSameDigit() {
+        List<Integer> actual = integerArrayService.defineThreeDigitNumberWithoutSameDigit(integerArray);
+        List<Integer> expect = List.of(831, 287);
+        assertEquals(actual, expect);
     }
 }
